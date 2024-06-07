06094deb9ea7202278cfac58994231e990f21bab
MISSEN123 <m.patila@issenglobal.com>
2024-06-07 14:22:27 +0900

    Remove Branding CAPITAL

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -58,7 +58,7 @@ export const Badge = (props: Props) => {
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Issen Global'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'ISSEN GLOBAL'}</span>
           </a>
         </span>
       </Show>
